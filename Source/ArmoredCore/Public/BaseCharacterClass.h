ef9fcb2b077d980924f19439a66795c086450f8e
Jangjinhyeok <zero9936@gmail.com>
2025-01-24 17:43:02 +0900

    BaseCharacterClass 생성 및 상속

---
+++ Source/ArmoredCore/Public/BaseCharacterClass.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseCharacterClass.generated.h"
+
+UCLASS()
+class ARMOREDCORE_API ABaseCharacterClass : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABaseCharacterClass();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MaxHP;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CurrentHP;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float StaggerGauge;
+	
+};
